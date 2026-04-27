39d605bb9dad90e047e41844971d6332f77f00e4
fatima12d <fatimamouhamed586@gmail.com>
2026-04-27 20:51:20 +0000

    premier commit tp-git-spring

---
+++ src/main/java/sn/isi/tpgitspring/TpGitSpringApplication.java
@@ -0,0 +1,13 @@
+package sn.isi.tpgitspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpGitSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpGitSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sn/isi/tpgitspring/TpGitSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.isi.tpgitspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpGitSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
